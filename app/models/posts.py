c5d3d4bbfcb99fbce4d0508d4d946a2742ea4ca5
Luna <kottakcode@gmail.com>
2026-02-21 18:17:42 +0300

    feat(models) add User and Post

---
+++ app/models/posts.py
@@ -0,0 +1,49 @@
+import numbers
+
+from sqlalchemy import Column, ColumnElement, Integer, String, Boolean, text, ForeignKey
+from sqlalchemy.orm import relationship
+
+
+from app.models.mixins.status_mixin import StatusMixin
+from app.models.mixins.timestamp_mixin import TimeStampMixin
+from app.models.mixins.soft_delete_mixin import SoftDeleteMixin
+
+
+class Post(TimeStampMixin, SoftDeleteMixin, StatusMixin):
+
+    __tablename__ = "posts"
+
+    id = Column(
+        Integer,
+        primary_key=True,
+        nullable=False,
+    )
+
+    title = Column(
+        String,
+        nullable=False,
+    )
+
+    content = Column(
+        String,
+        nullable=False,
+    )
+
+    published = Column(
+        Boolean,
+        nullable=False,
+        server_default=text("TRUE"),
+    )
+
+    # foreign key
+    user_id = Column(
+        Integer,
+        ForeignKey("users.id", ondelete="CASCADE"),
+        nullable=False,
+    )
+
+    # relations:
+    owner = relationship("User", back_populates="posts")
+
+
+
---
+++ app/models/users.py
@@ -0,0 +1,43 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+
+
+from app.models.mixins.status_mixin import StatusMixin
+from app.models.mixins.timestamp_mixin import TimeStampMixin
+from app.models.mixins.soft_delete_mixin import SoftDeleteMixin
+
+
+
+class User(TimeStampMixin, SoftDeleteMixin, StatusMixin):
+
+    __tablename__ = "users"
+
+    id = Column(
+        Integer,
+        primary_key=True,
+        nullable=False,
+    )
+
+    username = Column(
+        String,
+        nullable=False,
+        unique=True,
+    )
+
+    password = Column(
+        String,
+        nullable=False,
+    )
+
+    email = Column(
+        String,
+        nullable=True,
+        unique=True,
+    )
+
+    # Relations:
+    posts = relationship(
+        "Post",
+        back_populates="owner",
+        cascade="all, delete-orphan"
+    )
