081e21df60db48a647cce7597b768b4c2d0750cf
Luna <kottakcode@gmail.com>
2026-02-25 13:43:56 +0300

    feat(posts) add schemas

---
+++ app/schemas/posts.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+from pydantic import BaseModel, Field
+
+from app.models.mixins.status_mixin import StatusEnum
+from app.schemas.users import UserDataFromDbSchema
+
+
+# see the data:
+class PostDataFromDbSchema(BaseModel):
+    id: int
+    title: str
+    content: str
+    published: bool
+    created_at: datetime
+    deleted_at: datetime | None
+    status: StatusEnum = "ACTIVE"
+    owner: UserDataFromDbSchema
+
+# create a post:
+class PostCreationForm(BaseModel):
+    title: str = Field(
+        ...,
+        min_length=3,
+        max_length=80,
+        description="title of the post: 3 to 80 chars."
+    )
+
+    content: str = Field(
+        ...,
+        min_length=3,
+        max_length=2000,
+        description="content text of post: 3 to 2000 chars."
+    )
+
+    published: bool = True
